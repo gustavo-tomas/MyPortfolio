bf38fa7e28b8bec58d58742922c1d5c85ff4358a
Gustavo Tomás <guga.tomas10@gmail.com>
2022-05-08 19:15:49 -0300

    second try

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,100 +1,151 @@
 <template>
 <div id="container">
-  <Renderer
-    ref="renderer"
-    resize="window"
-    orbitCtrl
-    shadow
-  >
-    <Camera
-      ref="camera"
-      :far=20000
-      :position="{ x: -7, y: 19, z: -9 }"
-    />
-    <Scene
-      ref="scene"
-      background="#DEFEFF"
-    >
-      <DirectionalLight 
-        ref="dirLight"
-        color="#ffffff"
-        cast-shadow
-      />
-      <GltfModel
-        ref="room"
-        src="../../assets/models/room.gltf"
-        @load="onLoad"
-      />
-      <!-- <GltfModel
-        ref="coin"
-        src="../../assets/models/coin_embedded.gltf"
-        @load="onLoad"
-      /> -->
-      <GltfModel
-        ref="landscape"
-        :scale="{ x: 1.0, y: 1.0, z: 1.0 }"
-        src="../../assets/landscape.glb"
-        @load="onLoad"
-      />
-    </Scene>
-  </Renderer>
+  <canvas ref="bg"></canvas>
 </div>
 </template>
 
 <script>
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { Object3D } from "troisjs";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+// Loader
+const loader = new GLTFLoader();
+
+// Mixer
+let mixer;
 
 export default {
   name: "Canvas",
+  data() {
+    return {
+      loader: loader,
+      mixer: mixer,
+    };
+  },
   mounted() {
 
     // Scene setup
-    const scene = this.$refs.scene.scene;
+    const scene = new THREE.Scene();
 
-    // Loader
-    const loader = new GLTFLoader();
-
-    // Loading object
-    loader.load('../../assets/models/coin_embedded.gltf', (gltf) => {
-      const root = gltf.scene;
-
-      root.traverse((obj) => {
-        if (obj.isMesh) {
-          obj.castShadow = true;
-          obj.receiveShadow = true;
-        }
-      });
-      scene.add(root);
-    });
-
-    // Background setup
-    // const backgroundTexture = new THREE.TextureLoader().load("../../assets/frog.jpeg");
-    // scene.background = backgroundTexture;
-
-    // Directional light
-    const dirLight = this.$refs.dirLight.light;
-    dirLight.position.set(-1, 1.75, 1);
-    dirLight.intensity = 1.5;
+    // Camera setup
+    const fov = 75; 
+    const aspectRatio = window.innerWidth / window.innerHeight;
+    const near = 0.1;
+    const far = 1000;
+    const camera = new THREE.PerspectiveCamera(fov, aspectRatio, near, far);
+    camera.position.z = 30;
 
     // Renderer setup
-    // const renderer = this.$refs.renderer;
+    const renderer = new THREE.WebGL1Renderer({
+      canvas: this.$refs.bg,
+      antialias: true,
+      alpha: true,
+    });
+
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    renderer.shadowMap.type = THREE.PCFShadowMap;
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMapSoft = true;
+    renderer.outputEncoding = THREE.sRGBEncoding;
+
+    // Directional light
+    const dirLight = new THREE.DirectionalLight(0xFFFFFF);
+    dirLight.position.set(1, 10.75, 10);
+    dirLight.intensity = 1.5;
+    dirLight.castShadow = true;
+    scene.add(dirLight);
+
+    // Helpers
+    const lightHelper = new THREE.PointLightHelper(dirLight);
+    scene.add(lightHelper);
+
+    const gridHelper = new THREE.GridHelper(200, 50);
+    scene.add(gridHelper);
+
+    // Orbit controls
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    scene.background = new THREE.Color("#DEFEFF");
+
+    // Updates render on each frame
+    function animate() {
+      requestAnimationFrame(animate);
+      controls.update();
+      renderer.render(scene, camera);
+    }
+
+    // Plays specific animation
+    function playAnimation() {
+
+    }
+
+    // Updates the render size on window resize
+    function onWindowResize() {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    // Updates character position based on key pressed
+    function onKeyPress(e) {
+      const coin = scene.getObjectByName("Coin");
+
+      console.log(e.key, "key was pressed")
+      let valX = 0, valY = 0, valZ = 0;
+      if (e.key == 'a')
+        valX = -1;
+      else if (e.key == 'd')
+        valX = 1;
+      else if (e.key == 's')
+        valZ = 1;
+      else if (e.key == 'w')
+        valZ = -1;
+      
+      coin.position.x += valX;
+      coin.position.y += valY;
+      coin.position.z += valZ;
+
+      playAnimation();
+    }
+
+    // Loading objects
+    this.load('../../assets/models/coin.gltf', scene, true);
+    // this.load('../../assets/models/room_sep.glb', scene, false, true);
+    // this.load('../../assets/landscape.glb', scene, false, true);
+
+    window.addEventListener('resize', onWindowResize);
+    window.addEventListener('keydown', onKeyPress);
+    animate();
   },
   methods: {
-    onLoad(model) {
-      console.log("Model loaded", model);
-      if (typeof model != Object3D) {
-        console.log("Model is not of type 'Object3D'");
-        return;
-      }
-      model.traverse(function (child) {
-        if (child.isMesh) {
-          child.castShadow = true;
-          child.receiveShadow = true;
-        }
+    
+    // Loads GLTF objects with shadows disabled
+    load(path, scene, cast=false, rec=false) {
+      this.loader.load(path, (gltf) => {
+        const root = gltf.scene;
+        root.traverse((obj) => {
+          if (obj.isMesh) {
+            obj.castShadow = cast;
+            obj.receiveShadow = rec;
+          }
+        });
+        
+        this.mixer = new THREE.AnimationMixer(root);
+        const action = this.mixer.clipAction(gltf.animations[0]);
+        action.play();
+        
+        scene.add(root);
       });
     },
   },
 };
 </script>
+
+<style scoped>
+canvas {
+  display: block;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import { createApp } from 'vue'
-import { TroisJSVuePlugin } from 'troisjs';
 import App from './App.vue'
 import router from './router';
 
 const app = createApp(App);
 app.use(router);
-app.use(TroisJSVuePlugin);
 app.mount('#app');
