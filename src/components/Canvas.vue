d4dbaaebd40a3ff8db2774faa97b08bfd0add17d
Gustavo Tomás <guga.tomas10@gmail.com>
2022-05-09 10:17:35 -0300

    added fonts, animations and movement

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,6 +1,5 @@
 <template>
 <div id="container">
-  <canvas ref="bg"></canvas>
 </div>
 </template>
 
@@ -8,91 +7,143 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-
-// Loader
-const loader = new GLTFLoader();
-
-// Mixer
-let mixer;
+import Stats from "stats.js";
 
 export default {
   name: "Canvas",
-  data() {
-    return {
-      loader: loader,
-      mixer: mixer,
-    };
-  },
-  mounted() {
+  methods: {
+    init() {
 
-    // Scene setup
-    const scene = new THREE.Scene();
+      // Scene
+      this.scene = new THREE.Scene();
+      this.scene.background = new THREE.Color("#DEFEFF");
 
-    // Camera setup
-    const fov = 75; 
-    const aspectRatio = window.innerWidth / window.innerHeight;
-    const near = 0.1;
-    const far = 1000;
-    const camera = new THREE.PerspectiveCamera(fov, aspectRatio, near, far);
-    camera.position.z = 30;
+      // Camera
+      this.camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        10000
+      );
+      this.camera.position.set(1, 10, 10);
 
-    // Renderer setup
-    const renderer = new THREE.WebGL1Renderer({
-      canvas: this.$refs.bg,
-      antialias: true,
-      alpha: true,
-    });
+      // Renderer
+      this.renderer = new THREE.WebGLRenderer();
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      this.renderer.setPixelRatio(window.devicePixelRatio);
+      this.renderer.shadowMap.type = THREE.PCFShadowMap;
+      this.renderer.shadowMap.enabled = true;
+      this.renderer.shadowMapSoft = true;
+      this.renderer.outputEncoding = THREE.sRGBEncoding;
+      document.body.appendChild(this.renderer.domElement);
 
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
+      // Directional light
+      this.dirLight = new THREE.DirectionalLight(0xFFFFFF);
+      this.dirLight.position.set(1, 10.75, 10);
+      this.dirLight.intensity = 1.0;
+      this.dirLight.castShadow = true;
+      this.scene.add(this.dirLight);
 
-    renderer.shadowMap.type = THREE.PCFShadowMap;
-    renderer.shadowMap.enabled = true;
-    renderer.shadowMapSoft = true;
-    renderer.outputEncoding = THREE.sRGBEncoding;
+      // Helpers
+      this.lightHelper = new THREE.PointLightHelper(this.dirLight);
+      this.scene.add(this.lightHelper);
+      this.gridHelper = new THREE.GridHelper(200, 50);
+      this.scene.add(this.gridHelper);
 
-    // Directional light
-    const dirLight = new THREE.DirectionalLight(0xFFFFFF);
-    dirLight.position.set(1, 10.75, 10);
-    dirLight.intensity = 1.5;
-    dirLight.castShadow = true;
-    scene.add(dirLight);
+      // Orbit controls
+      this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+      
+      // Model loader
+      this.loader = new GLTFLoader();
+      this.loadGLTF('../../assets/models/coin.gltf', true);
+      this.loadGLTF('../../assets/models/room_sep.glb', false, true);
+      this.loadGLTF('../../assets/landscape.glb', false, true);
 
-    // Helpers
-    const lightHelper = new THREE.PointLightHelper(dirLight);
-    scene.add(lightHelper);
+      // Mixer
+      this.mixer = new THREE.AnimationMixer();
 
-    const gridHelper = new THREE.GridHelper(200, 50);
-    scene.add(gridHelper);
+      // Clock
+      this.clock = new THREE.Clock();
 
-    // Orbit controls
-    const controls = new OrbitControls(camera, renderer.domElement);
+      // Stats panel
+      this.stats = new Stats();
+      this.stats.showPanel(0);
+      document.body.appendChild(this.stats.dom);
 
-    scene.background = new THREE.Color("#DEFEFF");
+      // Text
+      this.loadGLTF('../../assets/fonts/lol.glb', true, true);
 
-    // Updates render on each frame
-    function animate() {
-      requestAnimationFrame(animate);
-      controls.update();
-      renderer.render(scene, camera);
-    }
+      // Event listeners 
+      window.addEventListener('resize', this.onWindowResize);
+      window.addEventListener('keypress', this.onKeyPress);
+      window.addEventListener('keyup', this.onKeyUp);
+    },
 
-    // Plays specific animation
-    function playAnimation() {
+    // Updates each frame
+    animate() {
+      requestAnimationFrame(this.animate);
+      this.stats.begin();
 
-    }
+      const delta = this.clock.getDelta();
+      this.mixer.update(delta);
 
-    // Updates the render size on window resize
-    function onWindowResize() {
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-      renderer.setSize(window.innerWidth, window.innerHeight);
-    }
+      this.renderer.render(this.scene, this.camera);
+      this.stats.end();
+    },
+
+    // Loads models and related properties
+    loadGLTF(path, cast=false, rec=false) {
+      this.loader.load(path, (gltf) => {
+        const model = gltf.scene;
+        
+        // Casting/receiving shadows
+        model.traverse((obj) => {
+          if (obj.isMesh) {
+            obj.castShadow = cast;
+            obj.receiveShadow = rec;
+          }
+        });
+
+        // Loading animations
+        this.animations = gltf.animations;
+        this.mixer = new THREE.AnimationMixer(model);
+
+        this.scene.add(model);
+      });
+    },
+    
+    // Play animation
+    playAnimation() {
+      // const player = this.scene.getObjectByName("Coin");
+      if (this.animations.length > 0) {
+        const action = this.mixer.clipAction(this.animations[0]);
+        action.play();
+        // this.animate();
+      } else {
+        console.log("No animations to play");
+      }
+    },
+
+    // Stop animation
+    stopAnimation() {
+      if (this.animations.length > 0) {
+        const action = this.mixer.clipAction(this.animations[0]);
+        action.stop();
+      } else {
+        console.log("No animations to stop");
+      }
+    },
+
+    // Updates renderer size on resize
+    onWindowResize() {
+      this.camera.aspect = window.innerWidth / window.innerHeight;
+      this.camera.updateProjectionMatrix();
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+    },
 
     // Updates character position based on key pressed
-    function onKeyPress(e) {
-      const coin = scene.getObjectByName("Coin");
-
+    onKeyPress(e) {
+      const player = this.scene.getObjectByName("Coin");
       console.log(e.key, "key was pressed")
       let valX = 0, valY = 0, valZ = 0;
       if (e.key == 'a')
@@ -104,48 +155,28 @@ export default {
       else if (e.key == 'w')
         valZ = -1;
       
-      coin.position.x += valX;
-      coin.position.y += valY;
-      coin.position.z += valZ;
-
-      playAnimation();
-    }
-
-    // Loading objects
-    this.load('../../assets/models/coin.gltf', scene, true);
-    // this.load('../../assets/models/room_sep.glb', scene, false, true);
-    // this.load('../../assets/landscape.glb', scene, false, true);
-
-    window.addEventListener('resize', onWindowResize);
-    window.addEventListener('keydown', onKeyPress);
-    animate();
-  },
-  methods: {
-    
-    // Loads GLTF objects with shadows disabled
-    load(path, scene, cast=false, rec=false) {
-      this.loader.load(path, (gltf) => {
-        const root = gltf.scene;
-        root.traverse((obj) => {
-          if (obj.isMesh) {
-            obj.castShadow = cast;
-            obj.receiveShadow = rec;
-          }
-        });
-        
-        this.mixer = new THREE.AnimationMixer(root);
-        const action = this.mixer.clipAction(gltf.animations[0]);
-        action.play();
-        
-        scene.add(root);
-      });
+      player.position.x += valX;
+      player.position.y += valY;
+      player.position.z += valZ;
+      
+      this.playAnimation();
     },
+
+    // Updates movement animation on key release
+    onKeyUp(e) {
+      const moves = ['w', 'a', 's', 'd'];
+      console.log(e.key, "was released");
+      if (moves.includes(e.key)) {
+        this.stopAnimation();  
+      }
+    },
+  },
+  mounted() {
+    this.init();
+    this.animate();
   },
 };
 </script>
 
 <style scoped>
-canvas {
-  display: block;
-}
 </style>
